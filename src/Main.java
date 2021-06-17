cfbeab4972f36b4aee8e2477577d124eb2c43dac
Tchorti <tchelo.rock@gmail.com>
2021-06-17 18:49:06 -0300

    Exercicio Saida de dados Java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	
+			
+			
+		String escola = "Vicente Leporace";
+		int numero = 133;
+		String bairro = "Jardim das Flores";
+		double curso = 1500;
+		
+		System.out.printf("%s fica no número %d no bairro %s e o valor do curso é %.2f reais por mês %n", escola, numero, bairro, curso);
+						
+		}
+		
+	}
+
+
